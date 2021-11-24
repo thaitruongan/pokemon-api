57edfaabcd3440406ad37cf1356be50e4fb59250
pntn070599@gmail.com <pntn070599@gmail.com>
2021-11-24 08:37:39 +0700

    add new lib 'node-cache', define cache-controller.js, define middleware auth.js

---
+++ controller/cache-controller.js
@@ -0,0 +1,43 @@
+const NodeCache = require("node-cache");
+
+module.exports = class CacheController {
+  constructor(ttlSeconds = 60) {
+    this.cache = new NodeCache({
+      stdTTL: ttlSeconds,
+      checkperiod: 30,
+      useClone: false,
+    });
+  }
+
+  get(key, storeFunction) {
+    const value = this.cache.get(key);
+    if (value) {
+      console.log(`Retrieving ${key} from cache`);
+      return Promise.resolve(value);
+    }
+
+    return storeFunction().then((result) => {
+      this.cache.set(key, result);
+      return result;
+    });
+  }
+
+  request(key) {
+    const value = this.cache.get(key);
+
+    if (value) {
+      console.log(`Retrieving ${key} from cache`);
+      return Promise.resolve(value);
+    }
+
+    return Promise.resolve(null);
+  }
+
+  delete(keys) {
+    this.cache.delete(keys);
+  }
+
+  flush() {
+    this.cache.flushAll();
+  }
+};
